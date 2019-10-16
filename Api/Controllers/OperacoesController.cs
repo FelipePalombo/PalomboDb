c0a7671dbb964e27de67dc4fda4dd8cbbac0aee7
Felipe R. Palombo <feliperibeiro.rp@gmail.com>
2019-10-16 20:10:27 -0300

    Estipulando arquitetura e referencias.

---
+++ Api/Controllers/OperacoesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("operacoes/[controller]")]
+    [ApiController]
+    public class OperacoesController : ControllerBase
+    {
+        // GET operacoes/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
