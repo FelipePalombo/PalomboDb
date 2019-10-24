099ee5d35822cf9deb24513389f0d7b2a2ff8719
Felipe R. Palombo <feliperibeiro.rp@gmail.com>
2019-10-23 21:11:03 -0300

    Adicionando camada de inversão de controle.

---
+++ IoC/IoCGeral.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IoC
+{
+    public class IoCGeral
+    {
+        public static void ConfigurarRepositorio(IServiceCollection services){
+            
+        }
+
+        public static void ConfigurarServico(IServiceCollection services){
+            
+        }
+    }
+}
