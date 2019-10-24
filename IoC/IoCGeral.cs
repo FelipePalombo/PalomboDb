c5e47216ee2c9a6e8f6990d819b74e3ba4f475e4
Felipe R. Palombo <feliperibeiro.rp@gmail.com>
2019-10-23 21:37:27 -0300

    Criando inversao de controle das classes de operacoes.

--- IoC/IoCGeral.cs
+++ IoC/IoCGeral.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using Microsoft.Extensions.DependencyInjection;
+using Servico.Interface;
+using Servico.Implementacao;
+using Repositorio.Interface;
+using Repositorio.Implementacao;
 
 namespace IoC
 {
     public class IoCGeral
     {
         public static void ConfigurarRepositorio(IServiceCollection services){
-            
+            services.AddScoped<IOperacoesServico,OperacoesServico>();
         }
 
         public static void ConfigurarServico(IServiceCollection services){
-            
+            services.AddScoped<IOperacoesRepositorio, OperacoesRepositorio>();
         }
     }
 }
