688fc19c9f9c01e3691e643a75ff543a16584f37
Felipe R. Palombo <feliperibeiro.rp@gmail.com>
2019-11-20 01:53:43 -0300

    Utilizando nova nomenclatura.

--- Dominio/TransacaoDominio.cs
+++ Dominio/TransacaoDominio.cs
@@ -5,7 +5,7 @@ namespace Dominio
     public class TransacaoDominio
     {
         public int Tid { get; set; }
-        public string Path { get => $"..\\Repositorio\\Banco\\Transacoes\\trans_{Tid}.json"; }
+        public string Path { get => $"..\\Repositorio\\Banco\\Processos\\trans_{Tid}.json"; }
         public int Status { get; set; }
     }
 }
